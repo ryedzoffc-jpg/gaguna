5063b2f1d8ea4ee8596b658de59b03d09d5a5c2c
RyedzOffc <ryedzoffc@gmail.com>
2026-04-18 15:28:24 +0800

    Create api.js

---
+++ api.js
@@ -0,0 +1,17 @@
+// api.js
+// Konfigurasi Firebase dan layanan lainnya
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDlJwwL4qGleoiJ65vwfzmYyZ1OWDygnhw",
+  authDomain: "draznews.firebaseapp.com",
+  databaseURL: "https://draznews-default-rtdb.firebaseio.com",
+  projectId: "draznews",
+  storageBucket: "draznews.firebasestorage.app",
+  messagingSenderId: "169063075153",
+  appId: "1:169063075153:web:b3cd52d3f34d768dafd770",
+  measurementId: "G-2P48317DPY"
+};
+
+// Jika ada integrasi Telegram Bot, bisa ditambahkan di sini
+export const telegramBotToken = "YOUR_TELEGRAM_BOT_TOKEN"; // opsional
+export const telegramChatId = "YOUR_CHAT_ID"; // opsional
